6235fd40edd266b3158cd70d222422bfbfd30486
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-01-28 10:43:58 +0500

    edit prime-games

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,7 +7,7 @@ const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-  for (let i = 2; i < num; i += i) {
+  for (let i = 2; i <= num; i += 1) {
     if (num % i === 0) {
       return false;
     }
